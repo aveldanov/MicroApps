83170a0c15a88557a6ddae826254d3b3e9d9c730
Anton Veldanov <anton.veldanov@gmail.com>
2020-10-09 23:23:19 -0700

    iOS

--- SwagShopper/SwagShopper/Services/DataService.swift
+++ SwagShopper/SwagShopper/Services/DataService.swift
@@ -10,4 +10,17 @@ import Foundation
 class DataService{
     static let instance = DataService() // singleton
     
+    private let categories = [
+        Category(title: "Shirts", imageName: "shirts.png"),
+        Category(title: "Hoodies", imageName: "hoodies.png"),
+        Category(title: "Hats", imageName: "hats.png"),
+        Category(title: "Digital", imageName: "digital.png")
+    ]
+    
+    
+    func getCategories(){
+        
+        
+        
+    }
 }
