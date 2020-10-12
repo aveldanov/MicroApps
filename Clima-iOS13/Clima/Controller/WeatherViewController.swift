6df22a0aed64e00430087201a8b0fd15d35927a1
Anton Veldanov <anton.veldanov@gmail.com>
2020-10-12 15:58:15 -0700

    iOS

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,50 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UISearchTextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self // textField reports back to current view controller
     }
-
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        }else{
+            textField.placeholder = "type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) { //method gets called by class through delegate which was set as WeatherViewController
+        // use textField
+        
+        
+        
+        searchTextField.text = ""
+    }
 
 }
 
